bfe7497ebdfc9cd58cb01734ba39395840518e32
aesd <aesd@Ubuntu.myguest.virtualbox.org>
2026-06-08 20:20:56 -0400

    Update assignment8 source revision

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -4,7 +4,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = '32ac72ac48d65caeed4213458f8914c74901712e'
+AESD_ASSIGNMENTS_VERSION = '077d34c241639d5f3168d6a08896470c16a699cc'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-emlinuxst.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
